9c7a064ab77c76608807fd6e2f33f1d1cb3ba13b
cjrojas72 <cjrojas72@gmail.com>
2016-03-30 18:39:34 -0400

    updated swapPhoto function
    added some jquery code

--- js/gallery.js
+++ js/gallery.js
@@ -37,6 +37,7 @@ function swapPhoto() {
 	//Access the img element and replace its source
 	//with a new image from your images array which is loaded 
 	//from the JSON string
+	$('#slideShow img').attr('src', 'mImges[mCrrentIndex]');
 	console.log('swap photo');
 }
 
