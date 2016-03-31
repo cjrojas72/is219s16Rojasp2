f73bd8bf74d38136debcb0e0d8b4ba22a9900f1f
cjrojas72 <cjrojas72@gmail.com>
2016-03-31 19:06:28 -0400

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -37,7 +37,13 @@ function swapPhoto() {
 	//Access the img element and replace its source
 	//with a new image from your images array which is loaded 
 	//from the JSON string
-	$('#slideShow img').attr('src', mImages[mCurrentIndex++]);
+	$('#slideShow img').attr('src', mImages[mCurrentIndex]);
+	mCurrentIndex++;
+	
+	if(mCurrentIndex === mImages.length)
+	{
+		mCurrentIndex = 0;	
+	}
 	console.log('swap photo');
 }
 
