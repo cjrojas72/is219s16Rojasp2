a9a856cb010dd5c4ef27a1c61fa2a8db50f58fbe
cjrojas72 <cjrojas72@gmail.com>
2016-04-03 12:03:37 -0400

    update details toggle

--- js/gallery.js
+++ js/gallery.js
@@ -169,6 +169,6 @@ $(document).ready(function()
 	$('.moreIndicator').click(function(){
 		$(this).toggleClass('rot270');
 		
-		$('.description').toggle();
+		$('.details, .description, .location, .date').toggle();
 	});
 });
